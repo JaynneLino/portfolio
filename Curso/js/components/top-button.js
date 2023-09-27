7b9be5af247fb854aa3f003d99fe19467de81ce4
JaynneLino <jaynne4@gmail.com>
2023-09-27 19:57:35 +0100

    Feat: hide or show page-up button on scroll

---
+++ Curso/js/components/top-button.js
@@ -0,0 +1,13 @@
+
+
+const topButton = document.querySelector('.top-button');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
